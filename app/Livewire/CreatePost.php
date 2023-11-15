35e1e371c058ae6816776f86c2a452eb42961c64
julicode97 <jlondono433@gmail.com>
2023-11-15 11:37:41 -0500

    5. componentes de pagina completa

--- app/Livewire/CreatePost.php
+++ app/Livewire/CreatePost.php
@@ -8,11 +8,6 @@ class CreatePost extends Component
 {
     public function render()
     {
-        // return view('livewire.create-post');
-        return <<<'HTML'
-            <div>
-                <h1>Hola desde el componente</h1>
-            </div>
-        HTML;
+        return view('livewire.create-post');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\CreatePost;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,7 +23,8 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    // Route::get('/dashboard', function () {
+    //     return view('dashboard');
+    // })->name('dashboard');
+    Route::get('/dashboard', CreatePost::class)->name('dashboard');
 });
